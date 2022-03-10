09a42ada8b4750e53d8f70a153ba8242c2408a63
Ceshack7 <95558934+Ceshack7@users.noreply.github.com>
2022-03-09 23:25:34 -0600

    Add files via upload

---
+++ z-virus.py
@@ -0,0 +1,49 @@
+import os
+import time
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+
+print(GREEN+"""
+██╗░░░██╗██╗██████╗░██╗░░░██╗░██████╗░░░░░░
+██║░░░██║██║██╔══██╗██║░░░██║██╔════╝░░░░░░
+╚██╗░██╔╝██║██████╔╝██║░░░██║╚█████╗░█████╗
+░╚████╔╝░██║██╔══██╗██║░░░██║░╚═══██╗╚════╝
+░░╚██╔╝░░██║██║░░██║╚██████╔╝██████╔╝░░░░░░
+░░░╚═╝░░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═════╝░░░░░░░
+███████╗
+╚════██║
+░░███╔═╝
+██╔══╝░░
+███████╗
+╚══════╝""")
+print(YELLOW+"--------------------------------------------------------")
+print(YELLOW+"Author : Ceshack7")
+print("--------------------------------------------------------")
+time.sleep(1)
+print("redes sociales : TikTok Github")
+print("--------------------------------------------------------")
+time.sleep(1)
+print("@Ceshack7 @SoyCeshack7 Git : Ceshack7")
+print("---------------------------------------------------------")
+time.sleep(1)
+print("Contacto : @SoyCeshack7")
+print("--------------------------------------------------------")
+time.sleep(1)
+print("Derechos de Author: Ceshack7")
+print("-------------------------------------------------------- \n")
+
+try:
+	number= int(input(WHITE+"ingresa el numero de telefono >>>"))
+except:
+		print("ha ocurrido un error")
+				
+for i in range(100000000000000000000000837473930303):
+	print(RED+"enviando virus")
